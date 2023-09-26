578db10768aa132f52d2d683b5ea3f311bd5311a
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-09-26 16:37:38 +0530

    Blog Tracker Application

---
+++ BlogTrackerMVC/BlogTrackerTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using BlogTrackerMVC.Models;
+
+namespace BlogTrackerMVC.Tests
+{
+    [TestFixture]
+    public class BlogTrackerTests
+    {
+        [Test]
+        public void BlogInfoIdTest()
+        {
+            var blog = new BlogInfo { BlogId = 1 };
+            var blogIdTest = blog.BlogId;
+            Assert.AreEqual(1, blogIdTest);
+        }
+
+        [Test]
+        public void BlogInfoTitleTest()
+        {
+            var blog = new BlogInfo { Title = "Test" };
+            var blogTitleTest = blog.Title;
+            Assert.AreEqual("Test", blogTitleTest);
+        }
+
+        [Test]
+        public void EmpInfoIdTest()
+        {
+            var emp = new EmpInfo { Id = 1 };
+            var empIdTest = emp.Id;
+            Assert.AreEqual(1, empIdTest);
+        }
+
+        [Test]
+        public void EmpInfoNameTest()
+        {
+            var emp = new EmpInfo { Name = "Name" };
+            var empNameTest = emp.Name;
+            Assert.AreEqual("Name", empNameTest);
+        }
+    }
+}
